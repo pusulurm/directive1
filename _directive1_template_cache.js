ef5f141fa2626244a15e84022074e67456bf47d0
pusulurm <murali.pusuluri@cdk.com>
2015-09-26 02:11:43 +0530

    changed html template

--- _directive1_template_cache.js
+++ _directive1_template_cache.js
@@ -1,5 +1,5 @@
 angular.module('directive1.templates', []).run(['$templateCache', function($templateCache) {
   "use strict";
   $templateCache.put("../templates/directive1.html",
-    "<hr><h2>Hello from Directive 1</h2>{{message1}}<label translate=DIRECTIVE1.KEY1></label><label translate=DIRECTIVE1.KEY2></label><hr>");
+    "<hr><h2>Hello from Directive 1</h2>{{message1}}<br><label translate=DIRECTIVE1.KEY1></label><br><label translate=DIRECTIVE1.KEY2></label><br><hr>");
 }]);
