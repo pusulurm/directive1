5ebae08f5d356c62d88e6da30b126cca8307ce6c
unknown <pusulurm@HYD-PUSULURM.ds.ad.adp.com>
2015-09-30 16:38:53 +0530

    Added localization cache module

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /**
  * Created by pusulurm on 9/24/2015.
  */
-define(['./myDirective1','./_directive1_template_cache'],function( directive1){
-    angular.module('Directive1Module', ['directive1.templates']).
+define(['./myDirective1','./_directive1_template_cache','./directive1_locale_cache'],function( directive1){
+    angular.module('Directive1Module', ['directive1.templates','directive1.i18n']).
         directive('myDirective1', directive1)
 });
